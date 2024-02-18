f3de8cf890195062276326f4e3ec51352f337dfc
Josh Lee <jleedev@gmail.com>
2024-02-18 17:59:33 -0500

    draw

---
+++ path.js
@@ -0,0 +1,50 @@
+export const path = () => {
+  let cx;
+  let pr = 4.5;
+  const handler = {
+    FeatureCollection(geom) {
+      geom.features.forEach(feat => this.Feature(feat));
+    },
+    Feature(feat) {
+      const { geometry } = feat;
+      if (geometry !== null) this[geometry.type](geometry);
+    },
+    GeometryCollection(geom) {
+      geom.geometries.forEach(geometry => this[geometry.type](geometry));
+    },
+    Point(geom) {
+      const { coordinates } = geom;
+      if (!coordinates.length) return;
+      const [x, y] = coordinates;
+      cx.ellipse(x, y, pr, pr, 0, 0, 2 * Math.PI, false);
+    },
+    MultiPoint(geom) {
+      geom.coordinates.forEach(coordinates => this.Point({ coordinates }));
+    },
+    LineString(geom) {
+      const { coordinates } = geom;
+      if (!coordinates.length) return;
+      coordinates.forEach(([x, y], i) => i ? cx.lineTo(x, y) : cx.moveTo(x, y));
+    },
+    MultiLineString(geom) {
+      geom.coordinates.forEach(coordinates => this.LineString({ coordinates }));
+    },
+    Polygon(geom) {
+      geom.coordinates.forEach(coordinates => this.LineString({ coordinates }));
+    },
+    MultiPolygon(geom) {
+      geom.coordinates.forEach(coordinates => this.Polygon({ coordinates }));
+    },
+  };
+  const handle = (geom) => handler[geom.type](geom);
+  return Object.assign(handle, {
+    context(value) {
+      cx = value;
+      return this;
+    },
+    pointRadius(value) {
+      pr = value;
+      return this;
+    },
+  });
+};
