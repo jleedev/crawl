90ec636fcb8389f72574f75405c1bd7ab1dd64a6
Josh Lee <jleedev@gmail.com>
2024-02-20 19:24:39 -0500

    prettier

--- polyfill.js
+++ polyfill.js
@@ -1,4 +1,10 @@
-if (!(Reflect.has(globalThis, "Iterator"))) {
-  const { default: value } = await import("https://esm.run/core-js-pure/actual/iterator");
-  Reflect.defineProperty(globalThis, "Iterator", { writable: true, configurable: true, value });
+if (!Reflect.has(globalThis, "Iterator")) {
+  const { default: value } = await import(
+    "https://esm.run/core-js-pure/actual/iterator"
+  );
+  Reflect.defineProperty(globalThis, "Iterator", {
+    writable: true,
+    configurable: true,
+    value,
+  });
 }
